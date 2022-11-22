4ec5d9ce28fb3a820321de701b67ff9b6aba4a95
see <seeflower.dev@gmail.com>
2022-11-22 11:34:28 +0800

    完全提交

--- link/perf_event.go
+++ link/perf_event.go
@@ -42,8 +42,7 @@ import (
 //   stops any further invocations of the attached eBPF program.
 
 var (
-	tracefsPath  = "/sys/kernel/debug/tracing"
-	tracefsPath2 = "/sys/kernel/tracing"
+	tracefsPath = "/sys/kernel/debug/tracing"
 
 	errInvalidInput = errors.New("invalid input")
 )
@@ -432,10 +431,4 @@ func isValidTraceID(s string) bool {
 	}
 
 	return true
-}
-
-func init() {
-	if _, err := os.Stat(tracefsPath); err != nil {
-		tracefsPath = tracefsPath2
-	}
 }
